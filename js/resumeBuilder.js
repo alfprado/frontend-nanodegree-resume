d0ddf6b1a8ef67fef2225b17a24343b6af6f4bbf
Prado André <prado_fernandes@yahoo.com.br>
2017-04-21 15:46:02 +0100

    Create objects bio, work, education and project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,96 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // bio contains pessoal information
+var bio = {
+	"name": "André Fernandes",
+	"role": "Web developer",
+	"contacts":{
+		"mobile": "+55 (12) 98175-5929",
+		"email": "prado_fernandes@yahoo.com.br",
+		"github":"alfprado",
+		"location": "Galway Ireland"
+	},
+	"pictureURl": "images/fry.jpg",
+	"welcomeMsg": "Hello!",
+	"skills": [
+		"HTML", "CSS", "Javascript"
+	]
+};
+
+// work contains experience information
+var work = {
+  "jobs": [
+    {
+      "employer": "Udacity",
+      "title": "Course Developer",
+      "location": "Mountain View, CA",
+      "dates": "Feb 2014 - Current",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LearnBIG",
+      "title": "Software Engineer",
+      "location": "Seattle, WA",
+      "dates": "May 2013 - Jan 2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LEAD Academy Charter High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jul 2012 - May 2013",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Stratford High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jun 2009 - Jun 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+};
+
+// education contains school and onlineCourses information
+var education = {
+	"schools": [{
+		"name": "IBTA",
+		"location": "Jacareí-SP",
+		"degree": "Bacharel",
+		"majors": ["CS"],
+		"dates": 2014,
+		"url": "http://example.com"
+	}, {
+		"name": "ITJ",
+		"location": "Jacareí-SP",
+		"degree": "Techinical",
+		"majors": ["CS"],
+		"dates": 2010,
+		"url": "http://example.com"
+	}],
+	"onlineCourses": [{
+		"title": "Introdução Java",
+		"school": "Caelum",
+		"dates": 2011,
+		"url": "http://www.caelum.com.br"
+	}]
+};
+
+// project contains information about projects executed
+var project = {
+	"projects": [{
+		"titles": "Sample Project 1",
+		"dates": "2014",
+		"description": "Who moved my cheese cheesy feet cauliflower cheese."
+	}, {
+		"titles": "Sample Project 2",
+		"dates": "2014",
+		"description": "Who moved my cheese cheesy feet cauliflower cheese."
+  }],
+	"images": [
+    "images/197x148.gif",
+    "images/197x148.gif"
+  ]
+};
